f4ab032507721a356e182c9523c39a1a5544b2b8
randy7771026 <randy7771026@gmail.com>
2016-09-25 17:21:10 -0500

    added popups
    popups revealed problem in filter logic so I fixed that

--- cs.js
+++ cs.js
@@ -8,6 +8,16 @@ var events = [];
 var mintime = 0;
 var maxtime = 0;
 
+ // create popup contents
+    var customPopup = "Mozilla Toronto Offices<br/><img src='http://joshuafrazier.info/images/maptime.gif' alt='maptime logo gif' width='350px'/>";
+    
+    // specify popup options 
+    var customOptions =
+        {
+        'maxWidth': '500',
+        'className' : 'custom'
+        }
+    
 
 var mymap = L.map('mapid').setView(coords, 13);
 
@@ -106,10 +116,10 @@ function make(test){
       maxtime = events[i].attributes.Time_Begun; 	
  	};
  	var time = moment(events[i].attributes.Time_Begun).format("MMM Do YY h:mm:ss a");
-    console.log("unix", events[i].attributes.Time_Begun, "time", time);
- 	if ( events[i].attributes.Premise_Type !== "Residence or House" ||
- 	     events[i].attributes.Premise_Type !== "Driveway" ||
-        events[i].attributes.Premise_Type !== "Road, Street, or Sidewalk" 	
+ //   console.log("unix", events[i].attributes.Time_Begun, "time", time);
+ 	if ( events[i].attributes.Premise_Type != "Residence or House" &&
+ 	     events[i].attributes.Premise_Type != "Driveway" &&
+        events[i].attributes.Premise_Type != "Road, Street, or Sidewalk" 	
  	 ) {
  	if ( events[i].attributes.SNB_No == 51 ||
  	     events[i].attributes.SNB_No == 45 ||
@@ -128,45 +138,51 @@ function make(test){
   
      var latlng = L.Projection.SphericalMercator.unproject(point);  
      
+     // create popup contents
+     customPopup =  events[i].attributes.Offense + "<br/>" +
+                    events[i].attributes.Premise_Type + "<br/>" +
+                    events[i].attributes.Address_Range + "<br/>" +
+                    time;
+     
      if ( events[i].attributes.Offense == "Theft" )
-     { 
-     new L.Marker([latlng.lat, latlng.lng], {icon: greenIcon},{bounceOnAdd: true}).addTo(mymap);
-     }
+      { 
+       new L.Marker([latlng.lat, latlng.lng], {icon: greenIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
+      }
      
       else if ( events[i].attributes.Offense == "Burglary" )
        { 
-        new L.Marker([latlng.lat, latlng.lng], {icon: blueIcon},{bounceOnAdd: true}).addTo(mymap);
+        new L.Marker([latlng.lat, latlng.lng], {icon: blueIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
        }
       
        
         else if( events[i].attributes.Offense == "Robbery" )
         { 
-         new L.Marker([latlng.lat, latlng.lng], {icon: redIcon},{bounceOnAdd: true}).addTo(mymap);
+         new L.Marker([latlng.lat, latlng.lng], {icon: redIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
         }
         
           else if( events[i].attributes.Offense == "Aggravated Assault" )
           { 
-           new L.Marker([latlng.lat, latlng.lng], {icon: violetIcon},{bounceOnAdd: true}).addTo(mymap);
+           new L.Marker([latlng.lat, latlng.lng], {icon: violetIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
           }
           
             else if( events[i].attributes.Offense == "Auto Theft" )
             { 
-             new L.Marker([latlng.lat, latlng.lng], {icon: blackIcon},{bounceOnAdd: true}).addTo(mymap);
+             new L.Marker([latlng.lat, latlng.lng], {icon: blackIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
             }
             
                else if( events[i].attributes.Offense == "Murder" )
                { 
-                new L.Marker([latlng.lat, latlng.lng], {icon: orangeIcon},{bounceOnAdd: true}).addTo(mymap);
+                new L.Marker([latlng.lat, latlng.lng], {icon: orangeIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
                }
                
                   else if( events[i].attributes.Offense == "Rape" )
                   { 
-                   new L.Marker([latlng.lat, latlng.lng], {icon: yellowIcon},{bounceOnAdd: true}).addTo(mymap);
+                   new L.Marker([latlng.lat, latlng.lng], {icon: yellowIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
                   }
         
         else 
         {
-         new L.Marker([latlng.lat, latlng.lng], {icon: greyIcon},{bounceOnAdd: true}).addTo(mymap);
+         new L.Marker([latlng.lat, latlng.lng], {icon: greyIcon},{bounceOnAdd: true}).bindPopup(customPopup,customOptions).addTo(mymap);
         }
      
      
